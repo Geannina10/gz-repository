8f5ef376c9f6993c5bb4bddc105b0f7ccdc15d03
Geannina Madrigal <geannina.10@gmail.com>
2023-09-22 00:17:15 +0000

    staging sales and product

--- models/sales.sql
+++ models/sales.sql
@@ -1,29 +1,29 @@
 {{ config(schema='transaction') }}
 
 WITH 
+sales AS (SELECT * FROM {{ ref('stg_sales') }} )
+ -- sales AS (SELECT * FROM `gz_raw_data.raw_gz_sales`)--
 
-  sales AS (SELECT * FROM `gz_raw_data.raw_gz_sales`)
-
-  ,product AS (SELECT * FROM `gz_raw_data.raw_gz_product`)
+  ,product AS (SELECT * FROM {{ ref('stg_product') }} )
 
 SELECT
   s.date_date
   ### Key ###
   ,s.orders_id
-  ,s.pdt_id AS products_id
+  ,s.products_id
   ###########
 	-- qty --
-	,s.quantity AS qty
+	,qty
   -- revenue --
-  ,s.revenue AS turnover
+  ,turnover
   -- cost --
-  ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
-	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS product_margin
+  ,CAST(p.purchase_price AS FLOAT64) AS purchase_price
+	,ROUND(s.qty*CAST(p.purchase_price AS FLOAT64),2) AS product_margin
 	-- margin --
-    ,{{ margin('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)') }} as margin
+    ,{{ margin('s.turnover', 's.qty*CAST(p.purchase_price AS FLOAT64)') }} as margin
 	--,s.revenue - s.quantity*CAST(p.purchSE_PRICE AS FLOAT64) AS margin--
-    ,{{ margin_percent('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)') }} as margin_percent
+    ,{{ margin_percent('s.turnover', 's.qty*CAST(p.purchase_price AS FLOAT64)') }} as margin_percent
 FROM sales s
-INNER JOIN product p ON s.pdt_id = p.products_id
+INNER JOIN product p ON s.products_id = p.products_id
 
 
